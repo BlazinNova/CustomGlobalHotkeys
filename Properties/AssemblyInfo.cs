ed2796f988555870c5ef220e8790a22036d1fc90
Aaron <44198148+whalehub@users.noreply.github.com>
2019-06-23 21:36:45 +0200

    Initial commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CustomGlobalHotkeys")]
+[assembly: AssemblyDescription("This plugin adds four custom global hotkeys to KeePass.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Aaron")]
+[assembly: AssemblyProduct("KeePass Plugin")]
+[assembly: AssemblyCopyright("Copyright © 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("b330cbd1-31a9-4542-8842-42fcea003c6c")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
